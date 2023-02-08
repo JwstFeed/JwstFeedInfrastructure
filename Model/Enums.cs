5b790b7e8cfe0a0dcd8f95638e4a941808d693a5
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-02-08 17:45:46 +0200

    Add files via upload

---
+++ Model/Enums.cs
@@ -0,0 +1,56 @@
+﻿namespace JwstFeedInfrastructure.Model;
+
+public enum eInputType
+{
+	HttpGet = 1,
+
+	HttpPost = 2,
+
+	ByUsername = 3,
+
+	MastPortal = 4
+}
+
+public enum eSourceType
+{
+	NasaBlogs = 1,
+
+	WebbTelescopeOrg = 2,
+
+	Flickr = 3,
+
+	Arxiv = 4,
+
+	Youtube = 5,
+
+	Reddit = 6,
+
+	StsciRawMiri = 7,
+
+	StsciSchedule = 8,
+
+	EsaWebb = 9,
+
+	Twitter = 10,
+
+	TwitterRawPhotoBot = 11,
+
+	StsciRawNiriss = 12,
+
+	WebbTelescopeImagesOrg = 13,
+
+	Ceers = 14,
+
+	StsciRawNircam = 15,
+
+	StsciRawNirspec = 16
+}
+
+public enum ePlotType
+{
+	Link = 0,
+
+	Image = 1,
+
+	Video = 2
+}
---
+++ Model/IFeedItem.cs
@@ -0,0 +1,22 @@
+﻿namespace JwstFeedInfrastructure.Model;
+
+public interface IFeedItem
+{
+	string ClusterIndex { get; }
+
+	DateTime DatePublished { get; }
+
+	ePlotType PlotType { get; }
+
+	string PlotUrl { get; }
+
+	string ShortTitle { get; }
+
+	eSourceType SourceType { get; }
+
+	string SourceUrl { get; }
+
+	string UniqueID { get; }
+
+	string ThumbnailUrl { get; }
+}
